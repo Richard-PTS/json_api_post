175d4a8d25aaec401096b78692a9caa9936c20ac
Richard Peterson Jr <richard@petersontechnology.solutions>
2020-11-10 10:44:21 -0600

    jsonlines support

--- api_json_post.py
+++ api_json_post.py
@@ -1,14 +1,14 @@
 from requests.auth import HTTPBasicAuth
 import requests
-from requests_jwt import JWTAuth
 import json
 from datetime import datetime
 import time
+import jsonlines # `pip install jsonlines`
 
 # Configuration variables, do not modify. Use config.json
 auth = ""
 api_url = ""
-json_file = ""
+jsonL_file = ""
 json_data = ""
 log_file = ""
 
@@ -53,12 +53,6 @@ def MakeRequest(request_data):
         print(r.request.headers)
         print('\n')
         LogWrite(r.text)
-    except urllib3.exceptions.ProtocolError:
-        LogWrite('EXCEPTION: urllib3.exceptions.ProtocolError')
-        res['status_code'] = 0
-    except requests.exceptions.HTTPError:
-        res['status_code'] = 0
-        LogWrite("EXCEPTION: request.exceptions.HTTPError")
     except:
         LogWrite("EXCEPTION: Unknown exemption")
 
@@ -74,21 +68,28 @@ with open('config.json', 'r') as config_file_data:
     config_data = json.load(config_file_data)
     auth = config_data[0]['auth-key']
     api_url = config_data[0]['apiURL']
-    json_file = config_data[0]['jsonFile']
+    jsonL_file = config_data[0]['jsonFile']
     log_file = config_data[0]['logFile']
 
 # Debug view
 LogWrite('SET: api_url:\t' + api_url)
-LogWrite('SET: json_file:\t' + json_file)
+LogWrite('SET: jsonL_file:\t' + jsonL_file)
 LogWrite('SET: log_file:\t' + log_file + '\n')
 
+with jsonlines.open(jsonL_file, "w") as reader:
+    for obj in reader:
+        print('\n')
+        print(obj)
+        print('\n')
+        break
+
 # Load the json data to send in the request
-with open(json_file, 'r') as json_file_data:
+with open(jsonL_file, 'r') as json_file_data:
     for line in json_file_data:
         json_data = json_data + line
 
 # Confirm critical information has been provided in the configuration file
-if (auth == "" or api_url == "" or json_file == ""):
+if (auth == "" or api_url == "" or jsonL_file == ""):
     # Log an error if something is missing
     LogWrite("Critical Information has not been defined in config.json. Request not sent!")
 else:
